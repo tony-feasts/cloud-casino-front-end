2e74fdbb5f591694795cee961de16feb25a49086
ChhaviDixit <chhavi1212@gmail.com>
2024-12-10 01:30:04 -0500

    Simple GraphQL implementation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { useState } from 'react';
 import { useGoogleLogin } from '@react-oauth/google';
+import { gql, useQuery } from "@apollo/client";
+
+
+const GET_DETAILS = gql`
+    query GetDetails {
+        players {
+            username
+        }
+    }
+`;
 
 function App() {
   const [showUsernamePrompt, setShowUsernamePrompt] = useState(false);
   const [username, setUsername] = useState('');
-
+  const { loading, error, data } = useQuery(GET_DETAILS);
   const googleLogin = useGoogleLogin({
     onSuccess: async (credentialResponse) => {
       console.log("Full credential response:", credentialResponse);
@@ -30,24 +40,28 @@ function App() {
   };
 
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>Online Casino</h1>
-      <button onClick={handleSignUpClick}>Sign Up</button>
-      <button style={{ marginLeft: '10px' }}>Log In (not implemented)</button>
-      
-      {showUsernamePrompt && (
-        <div style={{ marginTop: '20px' }}>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="Enter a username"
-            style={{ marginRight: '10px' }}
-          />
-          <button onClick={handleUsernameSubmit}>Sign in with Google</button>
+      <div style={{padding: '20px'}}>
+        <h1>Online Casino</h1>
+        <div style={{padding: '20px'}}>
+          {!error && !loading ? <span>Welcome! We have {{data}&&data.players.length} new players.</span>:''}
         </div>
-      )}
-    </div>
+
+        <button onClick={handleSignUpClick}>Sign Up</button>
+        <button style={{marginLeft: '10px'}}>Log In (not implemented)</button>
+
+        {showUsernamePrompt && (
+            <div style={{marginTop: '20px'}}>
+              <input
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  placeholder="Enter a username"
+                  style={{marginRight: '10px'}}
+              />
+              <button onClick={handleUsernameSubmit}>Sign in with Google</button>
+            </div>
+        )}
+      </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const client = new ApolloClient({
+    uri: 'http://localhost:5000/graphql', // Replace with your GraphQL endpoint
+    cache: new InMemoryCache(),
+});
 root.render(
   <GoogleOAuthProvider clientId="747809132155-su6a3kum44eabtir69jst8v9v1iasp98.apps.googleusercontent.com">
-    <App />
+      <ApolloProvider client = {client}>
+          <App />
+      </ApolloProvider>
+
   </GoogleOAuthProvider>
 );
